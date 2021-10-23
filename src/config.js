92b18f70897c63f8d192397357d860b126bc2d86
Tommy Huang <13917078913@163.com>
2021-10-23 20:05:32 +0800

    use https

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
-export const prefix=`http://localhost:8082/`;
+// export const prefix=`http://localhost:8082/`;
 
-export const prefix_2=`localhost:8082`;
+export const prefix = `https://localhost:8443/bookstore/`;
+
+// export const prefix_2 = `localhost:8082`;
+
+export const prefix_2 = `localhost:8082/bookstore`;
 
